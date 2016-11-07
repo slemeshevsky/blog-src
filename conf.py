9d98e525bc7ae232035bb4dd743661e33638e81e
Sergey Lemeshevsky <sergey.lemeshevsky@gmail.com>
2016-11-07 19:02:04 +0300

    Добавлена таблица

--- conf.py
+++ conf.py
@@ -170,6 +170,8 @@ extensions = [
     'alabaster',
     'ablog',
 	'sphinx_fontawesome',
+	'sphinxcontrib.exceltable',
+	'sphinxcontrib.email',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
